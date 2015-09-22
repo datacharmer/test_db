d2893c8280eaa0d2f7e54348c9b197da8eed6acf
Giuseppe Maxia <g.maxia@gmail.com>
2015-09-22 10:09:43 +0200

    Added view with the current dept of each employee

--- employees.sql
+++ employees.sql
@@ -95,6 +95,18 @@ CREATE TABLE salaries (
 ) 
 ; 
 
+CREATE OR REPLACE VIEW dept_emp_latest_date AS
+    SELECT emp_no, MAX(from_date) AS from_date, MAX(to_date) AS to_date
+    FROM dept_emp
+    GROUP BY emp_no;
+
+# shows only the current department for each employee
+CREATE OR REPLACE VIEW current_dept_emp AS
+    SELECT l.emp_no, dept_no, l.from_date, l.to_date
+    FROM dept_emp d
+        INNER JOIN dept_emp_latest_date l
+        ON d.emp_no=l.emp_no AND d.from_date=l.from_date AND l.to_date = d.to_date
+
 flush /*!50503 binary */ logs;
 
 SELECT 'LOADING departments' as 'INFO';
