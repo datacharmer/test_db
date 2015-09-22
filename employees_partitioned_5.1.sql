fca5b774b7eec12f8fa488fc7129d8be2fc271db
Giuseppe Maxia <g.maxia@gmail.com>
2015-09-22 10:12:52 +0200

    Added missing semicolon

--- employees_partitioned_5.1.sql
+++ employees_partitioned_5.1.sql
@@ -177,7 +177,7 @@ CREATE OR REPLACE VIEW current_dept_emp AS
     SELECT l.emp_no, dept_no, l.from_date, l.to_date
     FROM dept_emp d
         INNER JOIN dept_emp_latest_date l
-        ON d.emp_no=l.emp_no AND d.from_date=l.from_date AND l.to_date = d.to_date
+        ON d.emp_no=l.emp_no AND d.from_date=l.from_date AND l.to_date = d.to_date;
 
 flush /*!50503 binary */ logs;
 
