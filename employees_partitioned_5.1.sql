3b2bd4a45cd2143ea0dfb24d83537c419124ba59
David Meth <26856017+me42th@users.noreply.github.com>
2024-03-12 12:29:49 -0300

    Update employees_partitioned_5.1.sql

--- employees_partitioned_5.1.sql
+++ employees_partitioned_5.1.sql
@@ -43,7 +43,7 @@ CREATE TABLE employees (
     birth_date  DATE            NOT NULL,
     first_name  VARCHAR(14)     NOT NULL,
     last_name   VARCHAR(16)     NOT NULL,
-    gender      ENUM ('M','F')  NOT NULL,    
+    gender      ENUM ('M','F','NB','T')  NOT NULL,    
     hire_date   DATE            NOT NULL,
     PRIMARY KEY (emp_no)
 );
